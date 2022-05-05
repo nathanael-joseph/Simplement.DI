0df3a980258ee8dea13acf2fb2dceac5bcd96b9e
nathanael joseph <nathanaeljy@outlook.com>
2022-05-05 20:28:56 +0300

    added basic benchmarking test

--- Simplement.DI/Simplement.DI.Console/Program.cs
+++ Simplement.DI/Simplement.DI.Console/Program.cs
@@ -1,14 +1,61 @@
 ﻿
 using Simplement.DI.CoreLib;
+using Simplement.DI.Console;
+using System.Diagnostics;
 
+Stopwatch stopwatch = new Stopwatch();
 
-Console.WriteLine( typeof(string).IsValueType); 
-
+Console.WriteLine("registering Stub with () => new Stub() ");
 Container container = ContainerFactory.CreateBuilder(builder =>
 {
-    builder.RegisterTransient<string>();
+    builder.RegisterTransient<Stub>(() => new Stub());
 }).Build();
 
-string s = container.Request<string>(); 
+const int AMOUNT = 10000;
+
+Stub[] stubs = new Stub[AMOUNT];
+for (int i = 0; i < stubs.Length; i++)
+{
+    stubs[i] = null;
+}
+
+stopwatch.Start();
+for(int i = 0; i < stubs.Length; i++)
+{
+    stubs[i] = new Stub();
+}
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed} to create {AMOUNT} stubs with new() ...");
+
+stopwatch.Start();
+for (int i = 0; i < stubs.Length; i++)
+{
+    stubs[i] = container.Request<Stub>();
+}
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed} to create {AMOUNT} stubs with container.Request<Stub>() ...");
 
 
+Console.WriteLine("registering Stub with Invoke(null) ");
+container = ContainerFactory.CreateBuilder(builder =>
+{
+    builder.RegisterTransient<Stub>(() => new Stub());
+}).Build();
+
+
+stopwatch.Restart();
+for (int i = 0; i < stubs.Length; i++)
+{
+    stubs[i] = new Stub();
+}
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed} to create {AMOUNT} stubs with new() ...");
+
+stopwatch.Start();
+for (int i = 0; i < stubs.Length; i++)
+{
+    stubs[i] = container.Request<Stub>();
+}
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.Elapsed} to create {AMOUNT} stubs with container.Request<Stub>() ...");
+Console.ReadKey();
---
+++ Simplement.DI/Simplement.DI.Console/Stub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplement.DI.Console
+{
+    internal class Stub
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Stub() 
+        {
+            Name = "Stub";
+            Age = 0;
+        }
+    }
+}
